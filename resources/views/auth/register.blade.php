905e8c2377d10cd9ec37c5fc4aaa4f0699f50f99
meili-top <meili.llorente.work@gmail.com>
2019-12-03 17:28:58 +0100

    Text Changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -64,7 +64,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    Sign up
                                 </button>
                             </div>
                         </div>
